7f05b143c1ecd7f24d85ea68fb0d5fc7820d9399
Husnain-Akbar <husnainakbar140@gmail.com>
2020-07-04 19:28:59 +0500

    Add project files.

---
+++ LetsEnd/Shared/Entities/MoviesGenres.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetsEnd.Shared.Entities
+{
+    public class MoviesGenres
+    {
+        public int MovieId { get; set; }
+        public int GenresId { get; set; }
+
+    }
+}
